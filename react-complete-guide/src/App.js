c432f7e34d2b42445d15eb919b20832eaa4dc298
Marcos Leon <marcos_leonr@outlook.com>
2020-07-07 00:29:39 -0500

    customize components with props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
+        <p>This is really working</p>
+        <Person name = "Marcos" age = "20"/>
+        <Person name = "Yamillete" age = "21">My hobbies: I love listening to music</Person>
+        <Person name = "Mariama" age = "19"/>
       </div>
     );
   }
